232ea36429c399c732c75a49c96ed023f8519f36
子非鱼 <w3ctech@qq.com>
2019-12-27 13:05:38 +0800

    fix reg

--- biyingSpider.py
+++ biyingSpider.py
@@ -5,7 +5,7 @@ local = time.strftime("%Y.%m.%d")
 url = 'http://cn.bing.com/'
 con = requests.get(url)
 content = con.text
-reg = r"(az/hprichbg/rb/.*?.jpg)"
+reg = r"(\/th\?.*?\.jpg)"
 a = re.findall(reg, content, re.S)[0]
 print(a)
 picUrl = url + a
